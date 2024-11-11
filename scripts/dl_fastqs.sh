5c2af5a31b20c839aac81fd2993a191c16aeecd4
Guillem Santamaria Aguilar <guillemsantamariaaguilar@MacBook-Pro-de-Guillem.local>
2024-11-11 09:23:20 +0100

    Remove dl_fastqs.sh

--- scripts/dl_fastqs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ../../data
-
-while read -r accession; do
-  echo "Downloading accession: $accession"
-  prefetch $accession
-  fasterq-dump $accession | gzip
-done < accessions.txt
